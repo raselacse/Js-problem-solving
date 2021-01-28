fc3ce66baaecd6ad5fe14b0dfe7f47ef2da1eb26
Md. Rasel Hossain <rasel.as.cse@gmail.com>
2021-01-28 08:13:54 +0600

    update variable name, condition and functionality

--- assignment.js
+++ assignment.js
@@ -1,82 +1,86 @@
 //https://github.com/raselacse/Js-problem-solving
 
-// function kilometerToMeter(kilometer){
-//     if(kilometer>=0){
-//         var meter = kilometer*1000;
-//         return meter;
-//     }
-//     else if(kilometer<0){
-//         return 'Length cannot be negative!'
-//     }
-// }
-//     var result = kilometerToMeter(12);
-//     console.log(result);
+function kilometerToMeter(kilometer){
+    if(kilometer>=0){
+        var meter = kilometer*1000;
+        return meter;
+    }
+    else if(kilometer<0){
+        return 'Length cannot be negative!'
+    }
+}
+    var calculateMeter = kilometerToMeter(12);
+    console.log(calculateMeter);
 
 
 
 
 
-// function budgetCalculator(watch, phone, laptop){
-//     if( watch >= 0 && phone >= 0 && laptop >= 0 ){
-//         var watchPrice = watch*50;
-//         var phonePrice = phone*100;
-//         var laptopPrice = laptop*500;
-//         var total = watchPrice + phonePrice + laptopPrice;
-//     }
-//     else{
-//         return "Pleas enter positive value" ;
-//     }
-//     return total;
-// }
-// var items = {watch:-1, phone:0, laptop:0};
-// var result = budgetCalculator(items.watch, items.phone, items.laptop);
-// console.log(result);
-
-
-
-
-
-// function hotelCost(days){
-//     var first = 0, second = 0, third = 0;
-//     for(var i=0; i<=days; i++){
-//         if(i<=10){
-//             var first = i*100;
-//         }
-//         else if(i>10 && i<=20){
-//             var second = (i-10)*80;
-//         }
-//         else if(i>20){
-//             var third = (i-20)*50;
-//        }
-//     }
-//     var total = first + second + third;
-//     if(total>0){
-//         return total;
-//     }
-//     //for error message
-//     return "sir " + days + " day it's not possible! Please tell me, How long have you been here?"
-// }
-// var cost = hotelCost(30);
-// console.log(cost);
-
-
-
-
-
-function megaFriend(friendsName){
-    if(friendsName == '' || friendsName == ' '){
-        return 'we can not find becasue it is empty string';
+function budgetCalculator(watch, phone, laptop){
+    if( watch >= 0 && phone >= 0 && laptop >= 0 ){
+        var watchPrice = watch*50;
+        var phonePrice = phone*100;
+        var laptopPrice = laptop*500;
+        var budgetAmount = watchPrice + phonePrice + laptopPrice;
     }
     else{
-        var large = '';
-        for(var i=0; i<friendsName.length; i++){
-            var element = friendsName[i];
-            if(element.length>large.length){
-                large = element;
+        return "Pleas enter positive value" ;
+    }
+    return budgetAmount;
+}
+var items = {watch:-1, phone:0, laptop:0};
+var totalCost = budgetCalculator(items.watch, items.phone, items.laptop);
+console.log(totalCost);
+
+
+
+
+function hotelCost(days){
+    var firstTenDays = 0, secondTenDays = 0, afterTwentyDays = 0;
+    
+    if(days>0){
+        if(days<=10){
+            var firstTenDays = days*100;
+        }
+        if(days>10 && days<=20){
+            var firstTenDays = (days-10)*80;
+        }
+        if(days>20){
+            var firstTenDays = (days-20)*50;
+        }
+        var totalCost = firstTenDays + secondTenDays + afterTwentyDays;
+        return totalCost;
+    }
+    else{
+        return "sir " + days + " day it's not possible! Please tell me, How long have you been here?"
+    }
+}
+var calculateHotelCost = hotelCost(2);
+console.log(calculateHotelCost);
+
+
+
+
+
+
+
+function megaFriend(friendsName) {
+    if (friendsName == '') {
+        return "we can not find becasue it is empty string";
+    }
+    else if (friendsName == ' ') {
+        return "we can not find becasue it is blank string";
+    }
+    else {
+        var largestName = '';
+        for (var i = 0; i < friendsName.length; i++) {
+            var name = friendsName[i];
+            if (name.length > largestName.length) {
+                largestName = name;
             }
         }
+        return largestName;
     }
-    return large;
 }
-var largestName = megaFriend(['rasel hossain','shawon ahmed','raju ahmed','mahamodur rahman','mansurol islam']);
-console.log(largestName);
+var calculateLargestName = megaFriend(['rasel hossain', 'shawon ahmed', 'raju ahmed', 'mahamodur rahman', 'mansurol islam']);
+console.log(calculateLargestName);
